b2c53452fc307ffd5a5c278e3508def69b04a7e3
Harindu Dilshan <harindudilshan95@gmail.com>
2018-03-31 18:32:12 -0600

    spinner

--- ENSF_Final/src/frontend/Login.java
+++ ENSF_Final/src/frontend/Login.java
@@ -2,6 +2,8 @@ package frontend;
 
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
 
 import javax.swing.JFrame;
@@ -34,11 +36,13 @@ public class Login {
 	private JLabel cancelBtn;
 	boolean loginPressed = false;
 	private Client client;
-	
+	private JLabel waiting;
+	private JLabel spinner;
 
 	/**
 	 * Create the application.
-	 * @param c 
+	 * 
+	 * @param c
 	 */
 	public Login(Client c) {
 		this.client = c;
@@ -52,22 +56,36 @@ public class Login {
 		frame = new JFrame();
 		frame.setUndecorated(true);
 		frame.setResizable(false);
-		frame.setBounds(100, 100, 623, 404);
+		frame.setBounds(100, 100, 617, 374);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
 
-		Font font = new Font(Font.SANS_SERIF,Font.TRUETYPE_FONT,12);
+		Font font = new Font(Font.SANS_SERIF, Font.TRUETYPE_FONT, 12);
 		username = new JTextField();
 		username.setFont(font);
 		username.setSelectionColor(new Color(190, 168, 160));
-	
+
 		username.setText("username");
 		username.selectAll();
+
+		spinner = new Spinner("");
+		spinner.setBounds(275, 146, 48, 48);
+		ImageIcon imageIcon = new ImageIcon(Login.class.getResource("/images/red-spinner2.gif")); // load the image to a
+																									// imageIcon
+		spinner.setIcon(imageIcon);
+		spinner.setDoubleBuffered(true);
+		imageIcon.setImageObserver(spinner);
+		frame.getContentPane().add(spinner);
+
+		waiting = new JLabel("");
+		waiting.setIcon(new ImageIcon(Login.class.getResource("/images/loginData.png")));
+		waiting.setBounds(212, 73, 196, 222);
+		frame.getContentPane().add(waiting);
 		username.setBackground(new Color(190, 168, 170));
 		username.setBorder(null);
 
-		username.setBounds(236, 142, 153, 19);
+		username.setBounds(236, 141, 153, 19);
 		frame.getContentPane().add(username);
 		username.setColumns(20);
 
@@ -76,7 +94,7 @@ public class Login {
 		password.setFont(font);
 		password.setBorder(null);
 		password.setBackground(new Color(190, 168, 170));
-		password.setBounds(236, 189, 153, 19);
+		password.setBounds(236, 188, 153, 19);
 		frame.getContentPane().add(password);
 
 		loginBtn = new JLabel("");
@@ -116,10 +134,9 @@ public class Login {
 				}
 			}
 
-			
 		});
-		loginBtn.setIcon(new ImageIcon("resources/images/loginbtn3.png"));
-		loginBtn.setBounds(236, 229, 147, 33);
+		loginBtn.setIcon(new ImageIcon(Login.class.getResource("/images/loginbtn1.png")));
+		loginBtn.setBounds(236, 228, 147, 33);
 		frame.getContentPane().add(loginBtn);
 
 		cancelBtn = new JLabel("");
@@ -137,31 +154,65 @@ public class Login {
 				password.setText("");
 			}
 		});
-		cancelBtn.setIcon(new ImageIcon("resources/images/logincancel1.png"));
-		cancelBtn.setBounds(236, 263, 147, 33);
+		cancelBtn.setIcon(new ImageIcon(Login.class.getResource("/images/logincancel1.png")));
+		cancelBtn.setBounds(236, 262, 147, 33);
 		frame.getContentPane().add(cancelBtn);
 
 		JLabel background = new JLabel();
-		background.setIcon(new ImageIcon("resources/images/login_page.png"));
-		background.setBounds(0, -12, 619, 386);
+		background.setIcon(new ImageIcon(Login.class.getResource("/images/login_page.png")));
+		background.setBounds(0, -13, 619, 386);
 		frame.getContentPane().add(background);
 	}
-	
-	public void displayMessage (String Message)
-	{
+
+	public void displayMessage(String Message) {
 		JOptionPane.showMessageDialog(null, Message, "Message", JOptionPane.OK_OPTION);
 	}
-	
+
 	private void login(User u) {
-		if(u == null) {
+		if (u == null) {
 			displayMessage("Login failed!");
-		}else {
-			if(u instanceof Student) {
+		} else {
+			if (u instanceof Student) {
 				displayMessage("User is a student");
-			}else if(u instanceof Professor){
-				
+			} else if (u instanceof Professor) {
+
 				displayMessage("User is a professor");
 			}
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login window = new Login(null);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	class Spinner extends JLabel {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+
+		public Spinner(String txt) {
+			super(txt);
+		}
+
+		@Override
+		protected void paintComponent(Graphics g) {
+			  super.paintComponent(g);
 		}
 	}
 }
